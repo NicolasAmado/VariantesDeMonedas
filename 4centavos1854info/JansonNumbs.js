5137ee9f7fb4e57f5af46d0858e3033ac8909cf3
Nick Greefpool <nickgreefpool@live.com>
2025-07-04 20:31:36 -0300

    some work on aesthetics and P10

--- 4centavos1854info/JansonNumbs.js
+++ 4centavos1854info/JansonNumbs.js
@@ -1,15 +1,15 @@
 const janson4centavos1854 = [
-    { "Variante": "CJ# 1.1.1", "Anverso": "A1", "Reverso": "R1", "P1": 1, "P2": "_", "P3": 0, "P4": "_", "P5": 1, "P6": "_", "P7": 1, "P8": "_", "P9": "_"},
-    { "Variante": "CJ# 1.1.2", "Anverso": "A2", "Reverso": "R2", "P1": 1, "P2": "_", "P3": 0, "P4": "_", "P5": 0, "P6": 0, "P7": "_", "P8": "_", "P9": "_"},
-    { "Variante": "CJ# 1.1.3", "Anverso": "A3", "Reverso": "R3", "P1": 1, "P2": "_", "P3": 0, "P4": "_", "P5": 0, "P6": 1, "P7": "_", "P8": "_", "P9": "_"},
-    { "Variante": "CJ# 1.1.4", "Anverso": "A4", "Reverso": "R1", "P1": 1, "P2": "_", "P3": 0, "P4": "_", "P5": 1, "P6": "_", "P7": 0, "P8": 0, "P9": "_"},
-    { "Variante": "CJ# 1.1.5", "Anverso": "A4", "Reverso": "R3", "P1": 1, "P2": "_", "P3": 0, "P4": "_", "P5": 1, "P6": "_", "P7": 0, "P8": 0, "P9": "_"},
-    { "Variante": "CJ# 1.2.1", "Anverso": "A5", "Reverso": "R1", "P1": 0, "P2": 1, "P3": "_", "P4": "_", "P5": "_", "P6": "_", "P7": "_", "P8": "_", "P9": 1},
-    { "Variante": "CJ# 1.2.2", "Anverso": "A5", "Reverso": "R4", "P1": 0, "P2": 1, "P3": "_", "P4": "_", "P5": "_", "P6": "_", "P7": "_", "P8": "_", "P9": 0},
-    { "Variante": "CJ# 1.2.3", "Anverso": "A6", "Reverso": "R1", "P1": 0, "P2": 0, "P3": "_", "P4": "_", "P5": "_", "P6": "_", "P7": "_", "P8": "_", "P9": "_"},
-    { "Variante": "CJ# 1.3.1", "Anverso": "A7", "Reverso": "R3", "P1": 1, "P2": "_", "P3": 0, "P4": "_", "P5": 1, "P6": "_", "P7": 0, "P8": 1, "P9": "_"},
-    { "Variante": "CJ# 1.4.1", "Anverso": "A8", "Reverso": "R3", "P1": 1, "P2": "_", "P3": 1, "P4": 0, "P5": "_", "P6": "_", "P7": "_", "P8": "_", "P9": "_"},
-    { "Variante": "CJ# 1.5.1", "Anverso": "A9", "Reverso": "R5", "P1": 1, "P2": "_", "P3": 1, "P4": 1, "P5": "_", "P6": "_", "P7": "_", "P8": "_", "P9": "_"}
+    { "Variante": "CJ# 1.1.1", "Anverso": "A1", "Reverso": "R1", "P1": 1, "P2": "_", "P3": 0, "P4": "_", "P5": 1, "P6": "_", "P7": 1, "P8": "_", "P9": "_", "P10": "_"},
+    { "Variante": "CJ# 1.1.2", "Anverso": "A2", "Reverso": "R2", "P1": 1, "P2": "_", "P3": 0, "P4": "_", "P5": 0, "P6": 0, "P7": "_", "P8": "_", "P9": "_", "P10": "_"},
+    { "Variante": "CJ# 1.1.3", "Anverso": "A3", "Reverso": "R3", "P1": 1, "P2": "_", "P3": 0, "P4": "_", "P5": 0, "P6": 1, "P7": "_", "P8": "_", "P9": "_", "P10": "_"},
+    { "Variante": "CJ# 1.1.4", "Anverso": "A4", "Reverso": "R1", "P1": 1, "P2": "_", "P3": 0, "P4": "_", "P5": 1, "P6": "_", "P7": 0, "P8": 0, "P9": "_", "P10": 0},
+    { "Variante": "CJ# 1.1.5", "Anverso": "A4", "Reverso": "R3", "P1": 1, "P2": "_", "P3": 0, "P4": "_", "P5": 1, "P6": "_", "P7": 0, "P8": 0, "P9": "_", "P10": 1},
+    { "Variante": "CJ# 1.2.1", "Anverso": "A5", "Reverso": "R1", "P1": 0, "P2": 1, "P3": "_", "P4": "_", "P5": "_", "P6": "_", "P7": "_", "P8": "_", "P9": 1, "P10": "_"},
+    { "Variante": "CJ# 1.2.2", "Anverso": "A5", "Reverso": "R4", "P1": 0, "P2": 1, "P3": "_", "P4": "_", "P5": "_", "P6": "_", "P7": "_", "P8": "_", "P9": 0, "P10": "_"},
+    { "Variante": "CJ# 1.2.3", "Anverso": "A6", "Reverso": "R1", "P1": 0, "P2": 0, "P3": "_", "P4": "_", "P5": "_", "P6": "_", "P7": "_", "P8": "_", "P9": "_", "P10": "_"},
+    { "Variante": "CJ# 1.3.1", "Anverso": "A7", "Reverso": "R3", "P1": 1, "P2": "_", "P3": 0, "P4": "_", "P5": 1, "P6": "_", "P7": 0, "P8": 1, "P9": "_", "P10": "_"},
+    { "Variante": "CJ# 1.4.1", "Anverso": "A8", "Reverso": "R3", "P1": 1, "P2": "_", "P3": 1, "P4": 0, "P5": "_", "P6": "_", "P7": "_", "P8": "_", "P9": "_", "P10": "_"},
+    { "Variante": "CJ# 1.5.1", "Anverso": "A9", "Reverso": "R5", "P1": 1, "P2": "_", "P3": 1, "P4": 1, "P5": "_", "P6": "_", "P7": "_", "P8": "_", "P9": "_", "P10": "_"}
 ];
 
 const decisionTree4centavos1854 = {
@@ -79,7 +79,17 @@ const decisionTree4centavos1854 = {
                                     aclaracion_no: "Sobre el 54, hay 3 rayos finos, como en el resto de la moneda.",
                                     imagen: "4centavos1854info/Pregunta8.jpg",
                                     loe: [
-                                        { name: "A4" }, // No
+                                        { // No
+                                            texto: "¿Cuatro alineada con la segunda N de Nacional?",
+                                            clave: "P10",
+                                            aclaracion_si: "Segunda N de Nacional alineada con la palabra Cuatro.",
+                                            aclaracion_no: "Segunda A de Nacional alineada con la palabra Cuatro (N desalineada).",
+                                            imagen: "4centavos1854info/Pregunta10.jpg",
+                                            loe: [
+                                                { name: "CJ# 1.1.4, A4 R1" }, // No
+                                                { name: "CJ# 1.1.5, A4 R3" }  // Sí
+                                            ]
+                                        },
                                         { name: "CJ# 1.3.1, A7 R3" }  // Sí
                                     ]
                                 },
