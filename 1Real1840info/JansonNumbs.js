778909cd0e3d2fdc48eef8d012d54bd9ecdb4a88
Nick Greefpool <nickgreefpool@live.com>
2025-07-05 19:31:44 -0300

    mas preguntas de 1 real

--- 1Real1840info/JansonNumbs.js
+++ 1Real1840info/JansonNumbs.js
@@ -1,24 +1,24 @@
 const janson1Real1840 = [
-    { "Variante": "CJ# 16.1.1", "Anverso": "A1", "Reverso": "R1", "P1": 1,  "P2": 0  , "P3": "_","P4": "_", "P5": 0  , },
-    { "Variante": "CJ# 16.1.2", "Anverso": "A2", "Reverso": "R1", "P1": 1,  "P2": 1  , "P3": "_","P4": 1  , "P5": "_", },
-    { "Variante": "CJ# 16.1.3", "Anverso": "A3", "Reverso": "R1", "P1": 0,  "P2": "_", "P3": 1,  "P4": "_", "P5": "_", },
-    { "Variante": "CJ# 16.1.4", "Anverso": "A3", "Reverso": "R3", "P1": 0,  "P2": "_", "P3": 1,  "P4": "_", "P5": "_", },
-    { "Variante": "CJ# 16.1.5", "Anverso": "A3", "Reverso": "R10", "P1": 0, "P2": "_", "P3": 1,  "P4": "_", "P5": "_", },
-    { "Variante": "CJ# 16.1.6", "Anverso": "A4", "Reverso": "R1", "P1": 0,  "P2": "_", "P3": 0,  "P4": "_", "P5": "_", },
-    { "Variante": "CJ# 16.1.7", "Anverso": "A4", "Reverso": "R3", "P1": 0,  "P2": "_", "P3": 0,  "P4": "_", "P5": "_", },
-    { "Variante": "CJ# 16.1.8", "Anverso": "A4", "Reverso": "R10", "P1": 0, "P2": "_", "P3": 0,  "P4": "_", "P5": "_", },
-    { "Variante": "CJ# 16.1.9", "Anverso": "A4", "Reverso": "R11", "P1": 0, "P2": "_", "P3": 0,  "P4": "_", "P5": "_", },
-    { "Variante": "CJ# 16.1.10", "Anverso": "A5", "Reverso": "R2", "P1": 1, "P2": 0, "P3": "_",  "P4": "_", "P5": 0  , },
-    { "Variante": "CJ# 16.1.11", "Anverso": "A6", "Reverso": "R4", "P1": 0, "P2": "_", "P3": 0,  "P4": "_", "P5": "_", },
-    { "Variante": "CJ# 16.1.12", "Anverso": "A6", "Reverso": "R8", "P1": 0, "P2": "_", "P3": 0,  "P4": "_", "P5": "_", },
-    { "Variante": "CJ# 16.1.13", "Anverso": "A8", "Reverso": "R6", "P1": 1, "P2": 0, "P3": "_",  "P4": "_", "P5": 0  , },
-    { "Variante": "CJ# 16.1.14", "Anverso": "A9", "Reverso": "R4", "P1": 1, "P2": 0, "P3": "_",  "P4": "_", "P5": 1,   },
-    { "Variante": "CJ# 16.1.15", "Anverso": "A9", "Reverso": "R7", "P1": 1, "P2": 0, "P3": "_",  "P4": "_", "P5": 1,   },
-    { "Variante": "CJ# 16.1.16", "Anverso": "A10", "Reverso": "R3", "P1": 1,"P2": 1, "P3": "_",  "P4": 0  , "P5": "_", },
-    { "Variante": "CJ# 16.1.17", "Anverso": "A10", "Reverso": "R8", "P1": 1,"P2": 1, "P3": "_",  "P4": 0  , "P5": "_", },
-    { "Variante": "CJ# 16.1.18", "Anverso": "A11", "Reverso": "R9", "P1": 0,"P2": "_", "P3": 1,  "P4": "_", "P5": "_", },
-    { "Variante": "CJ# 16.3.1", "Anverso": "A2", "Reverso": "R5", "P1": 1,  "P2": 1  , "P3": "_","P4": 1  , "P5": "_", },
-    { "Variante": "CJ# 16.4.1", "Anverso": "A7", "Reverso": "R5", "P1": 1,  "P2": 1, "P3": "_",  "P4": 1  , "P5": "_", }
+    { "Variante": "CJ# 16.1.1", "Anverso": "A1", "Reverso": "R1", "P1": 1,  "P2": 0  , "P3": "_","P4": "_", "P5": 0  , "P6": "_", "P7": "_", "P8": "_", "P9": "_", "P10": "_", "P11": "_" },
+    { "Variante": "CJ# 16.1.2", "Anverso": "A2", "Reverso": "R1", "P1": 1,  "P2": 1  , "P3": "_","P4": 1  , "P5": "_", "P6": "_", "P7": 1  , "P8": "_", "P9": "_", "P10": "_", "P11": "_" },
+    { "Variante": "CJ# 16.1.3", "Anverso": "A3", "Reverso": "R1", "P1": 0,  "P2": "_", "P3": 1,  "P4": "_", "P5": "_", "P6": "_", "P7": "_", "P8": "_", "P9": 0  , "P10": "_", "P11": "_" },
+    { "Variante": "CJ# 16.1.4", "Anverso": "A3", "Reverso": "R3", "P1": 0,  "P2": "_", "P3": 1,  "P4": "_", "P5": "_", "P6": "_", "P7": "_", "P8": "_", "P9": 0  , "P10": "_", "P11": "_" },
+    { "Variante": "CJ# 16.1.5", "Anverso": "A3", "Reverso": "R10", "P1": 0, "P2": "_", "P3": 1,  "P4": "_", "P5": "_", "P6": "_", "P7": "_", "P8": "_", "P9": 0  , "P10": "_", "P11": "_" },
+    { "Variante": "CJ# 16.1.6", "Anverso": "A4", "Reverso": "R1", "P1": 0,  "P2": "_", "P3": 0,  "P4": "_", "P5": "_", "P6": "_", "P7": "_", "P8": "_", "P9": "_", "P10": 1,   "P11": "_" },
+    { "Variante": "CJ# 16.1.7", "Anverso": "A4", "Reverso": "R3", "P1": 0,  "P2": "_", "P3": 0,  "P4": "_", "P5": "_", "P6": "_", "P7": "_", "P8": "_", "P9": "_", "P10": 1,   "P11": "_" },
+    { "Variante": "CJ# 16.1.8", "Anverso": "A4", "Reverso": "R10", "P1": 0, "P2": "_", "P3": 0,  "P4": "_", "P5": "_", "P6": "_", "P7": "_", "P8": "_", "P9": "_", "P10": 1,   "P11": "_" },
+    { "Variante": "CJ# 16.1.9", "Anverso": "A4", "Reverso": "R11", "P1": 0, "P2": "_", "P3": 0,  "P4": "_", "P5": "_", "P6": "_", "P7": "_", "P8": "_", "P9": "_", "P10": 1,   "P11": "_" },
+    { "Variante": "CJ# 16.1.10", "Anverso": "A5", "Reverso": "R2", "P1": 1, "P2": 0, "P3": "_",  "P4": "_", "P5": 0  , "P6": "_", "P7": "_", "P8": "_", "P9": "_", "P10": "_", "P11": "_" },
+    { "Variante": "CJ# 16.1.11", "Anverso": "A6", "Reverso": "R4", "P1": 0, "P2": "_", "P3": 0,  "P4": "_", "P5": "_", "P6": "_", "P7": "_", "P8": "_", "P9": "_", "P10": 0,   "P11": 1   },
+    { "Variante": "CJ# 16.1.12", "Anverso": "A6", "Reverso": "R8", "P1": 0, "P2": "_", "P3": 0,  "P4": "_", "P5": "_", "P6": "_", "P7": "_", "P8": "_", "P9": "_", "P10": 0,   "P11": 0   },
+    { "Variante": "CJ# 16.1.13", "Anverso": "A8", "Reverso": "R6", "P1": 1, "P2": 0, "P3": "_",  "P4": "_", "P5": 0  , "P6": "_", "P7": "_", "P8": "_", "P9": "_", "P10": "_", "P11": "_" },
+    { "Variante": "CJ# 16.1.14", "Anverso": "A9", "Reverso": "R4", "P1": 1, "P2": 0, "P3": "_",  "P4": "_", "P5": 1,   "P6": "_", "P7": "_", "P8": 0  , "P9": "_", "P10": "_", "P11": "_" },
+    { "Variante": "CJ# 16.1.15", "Anverso": "A9", "Reverso": "R7", "P1": 1, "P2": 0, "P3": "_",  "P4": "_", "P5": 1,   "P6": "_", "P7": "_", "P8": 1  , "P9": "_", "P10": "_", "P11": "_" },
+    { "Variante": "CJ# 16.1.16", "Anverso": "A10", "Reverso": "R3", "P1": 1,"P2": 1, "P3": "_",  "P4": 0  , "P5": "_", "P6": 1,   "P7": "_", "P8": "_", "P9": "_", "P10": "_", "P11": "_" },
+    { "Variante": "CJ# 16.1.17", "Anverso": "A10", "Reverso": "R8", "P1": 1,"P2": 1, "P3": "_",  "P4": 0  , "P5": "_", "P6": 0,   "P7": "_", "P8": "_", "P9": "_", "P10": "_", "P11": "_" },
+    { "Variante": "CJ# 16.1.18", "Anverso": "A11", "Reverso": "R9", "P1": 0,"P2": "_", "P3": 1,  "P4": "_", "P5": "_", "P6": "_", "P7": "_", "P8": "_", "P9": 1  , "P10": "_", "P11": "_" },
+    { "Variante": "CJ# 16.3.1", "Anverso": "A2", "Reverso": "R5", "P1": 1,  "P2": 1  , "P3": "_","P4": 1  , "P5": "_", "P6": "_", "P7": 0  , "P8": "_", "P9": "_", "P10": "_", "P11": "_" },
+    { "Variante": "CJ# 16.4.1", "Anverso": "A7", "Reverso": "R5", "P1": 1,  "P2": 1, "P3": "_",  "P4": 1  , "P5": "_", "P6": "_", "P7": 0  , "P8": "_", "P9": "_", "P10": "_", "P11": "_" }
 ];
 
 const decisionTree1Real1840 = {
@@ -35,7 +35,42 @@ const decisionTree1Real1840 = {
             aclaracion_no: "En el extremo la rama derecha, hay una hoja simple en la parte superior.",
             imagen: "1Real1840info/Pregunta3.jpg",
             loe: [
+                    { // No
+                        texto: "¿La moneda tiene hoja inferior doble en la rama derecha?",
+                        clave: "P10",
+                        aclaracion_si: "En el extremo la rama derecha, hay una hoja doble en la parte inferior.",
+                        aclaracion_no: "En el extremo la rama derecha, hay una hoja simple en la parte inferior.",
+                        imagen: "1Real1840info/Pregunta10.jpg",
+                        loe: [
+                            { // No
+                            texto: "¿La F de Federación está alineada con la hoja?",
+                            clave: "P11",
+                            aclaracion_si: "La F de Federación está alineada con la hoja en el reverso.",
+                            aclaracion_no: "La F de Federación desalineada con la hoja, en su lugar, la L de LA está alineada.",
+                            imagen: "1Real1840info/Pregunta11.jpg",
+                            loe: [
+                                { name: "CJ# 16.1.12, A6 R8" }, // No
+                                { name: "CJ# 16.1.11, A6 R4 " }  // Sí
+                            ]
+                            },
+                            { // si
 
+                            }
+                        ]
+                    },
+                    { // si
+                        texto: "¿La moneda tiene hoja inferior doble en la rama derecha?",
+                        clave: "P9",
+                        aclaracion_si: "En el extremo la rama derecha, hay una hoja doble en la parte inferior.",
+                        aclaracion_no: "En el extremo la rama derecha, hay una hoja simple en la parte inferior.",
+                        imagen: "1Real1840info/Pregunta9.jpg",
+                        loe: [
+                            { // No
+                            
+                            },
+                            { name: "CJ# 16.1.18, A11 R9" } // si
+                        ]
+                    }
             ]
         },
         { // Sí
@@ -49,27 +84,21 @@ const decisionTree1Real1840 = {
                     texto: "¿La palabra De está por delante del extremo superior de la rama izquierda?",
                     clave: "P5",
                     aclaracion_si: "DE por delante del final de la rama izquieda.",
-                    aclaracion_no: "De por detrás del final de la rama izquierda.",
+                    aclaracion_no: "DE por detrás del final de la rama izquierda.",
                     imagen: "1Real1840info/Pregunta5.jpg",
                     loe: [
                         { // No
-                            texto: "¿?",
-                            clave: "P6",
-                            aclaracion_si: ".",
-                            aclaracion_no: ".",
-                            imagen: "1Real1840info/Pregunta5.jpg",
-                            loe: [
-
-                            ]
+                            
                         },
-                        { // Sí
-                            texto: "¿?",
-                            clave: "P7",
-                            aclaracion_si: "s.",
-                            aclaracion_no: ".",
-                            imagen: "1Real1840info/Pregunta4.jpg",
+                        { // si
+                            texto: "¿El extremo derecho de la rama del reverso tiene sus hojas alineadas?",
+                            clave: "P8",
+                            aclaracion_si: "Las tres hojas de la rama derecha están alineadas.",
+                            aclaracion_no: "Una de las hojas de la rama derecha está desalineada.",
+                            imagen: "1Real1840info/Pregunta8.jpg",
                             loe: [
-
+                                { name: "CJ# 16.1.14, A9 R4" }, // No
+                                { name: "CJ# 16.1.15, A9 R7 " }  // Sí
                             ]
                         }
                     ]
@@ -81,7 +110,30 @@ const decisionTree1Real1840 = {
                     aclaracion_no: "Punto después de Moneda está lejos de la S de Ayres.",
                     imagen: "1Real1840info/Pregunta4.jpg",
                     loe: [
+                        { // No
+                            texto: "¿La I de Federación está debajo de la tercera hoja?",
+                            clave: "P6",
+                            aclaracion_si: "La I de federación está debajo de la tercera hoja en el reverso.",
+                            aclaracion_no: "La I de federación está sobre la tercera hoja en el reverso.",
+                            imagen: "1Real1840info/Pregunta6.jpg",
+                            loe: [
+                                { name: "CJ# 16.1.17, A10 R8" }, // No
+                                { name: "CJ# 16.1.16, A10 R3 " }  // Sí
+                            ]
+                        },
+                        { // Sí
+                            texto: "¿El reverso tiene pocas hojas?",
+                            clave: "P7",
+                            aclaracion_si: "El reverso tiene pocas hojas.",
+                            aclaracion_no: "El reverso tiene muchas hojas.",
+                            imagen: "1Real1840info/Pregunta7.jpg",
+                            loe: [
+                                { // No
 
+                                }, 
+                                { name: "CJ# 16.1.2, A2 R1 " }  // Sí
+                            ]
+                        }
                     ]
                 }
 
